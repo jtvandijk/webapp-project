6d71c23234445f4ca551a4baabc05936b96ccf87
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-25 15:25:19 +0100

    Update settings

--- webapp/settings.py
+++ webapp/settings.py
@@ -11,10 +11,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
