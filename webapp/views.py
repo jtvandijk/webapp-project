dedbe2f262695e340720614dc2166ee3522a9be2
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-30 16:22:40 +0100

    Django bug fixes

--- webapp/views.py
+++ webapp/views.py
@@ -2,24 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Sum
 from kde.models import KdeLookup
-import sys
 
 def index(request):
 
-    def str_to_class(classname):
-        return getattr(sys.modules[__name__], classname)
+    data_freqs=[]
+    data_freqs.append(KdeLookup.objects.filter(freq1998__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq1999__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2000__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2001__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2002__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2003__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2004__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2005__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2006__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2007__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2008__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2009__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2010__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2011__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2012__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2013__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2014__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2015__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2016__isnull=False).distinct().count())
+    data_freqs.append(KdeLookup.objects.filter(freq2017__isnull=False).distinct().count())
 
-    def distinct_count(column):
-        coln = str_to_class(column + (str(__isnull)))
-        dist_f = KdeLookup.objects.filter(coln=False).distinct().count()
-        return(dist_f)
-
-
-
-    f2017 = distinct_count('freq2017')
-
-    print(f2017)
-
-
-
-    return render(request, "index.html", {'data_freqs' : data_freqs})
+    return render(request, "index.html", {'data_freqs':data_freqs})
