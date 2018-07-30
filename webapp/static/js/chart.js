2c4ab6ed3834aa0708e38019991b1a2bcf03a150
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-30 14:57:05 +0100

    Django bug fixes

--- webapp/static/js/chart.js
+++ webapp/static/js/chart.js
@@ -7,7 +7,7 @@ data: {
            '2010', '2011', '2012','2013','2014','2015','2016','2017'],
     datasets: [{
       label: 'Frequency of surname: {{ search_sur }}',
-      data: {{freqs}},
+      data: '{{freqs}}',
       borderColor: "#3273d1",
       backgroundColor: "rgba(0,123,250,0.4)",
       pointBackgroundColor: "#000000",
