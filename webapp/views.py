28c709467bfc4bc160f3185da437a05734726fe9
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-30 16:15:00 +0100

    Django bug fixes

--- webapp/views.py
+++ webapp/views.py
@@ -6,20 +6,18 @@ import sys
 
 def index(request):
 
-    def str_to_class(classname):
-        return getattr(sys.modules[__name__], classname)
-
     def distinct_count(column):
-        coln = str_to_class(column + ('__isnull'))
+        coln = (column + ('__isnull'))
         dist_f = KdeLookup.objects.filter(coln=False).distinct().count()
         return(dist_f)
 
     data_freqs = []
     for i in range(1998,2017):
 
-        year = str_to_class('freq'+str(i))
+        year = 'freq'+str(i)
+        print(year)
         freq_y = distinct_count(year)
-        data_freqs.apend(freq_y)
+        data_freqs.append(freq_y)
 
     print(data_freqs)
 
