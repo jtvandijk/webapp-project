6e7d0701cf919aa54ae6672fdb6869960a8772e4
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-30 16:05:00 +0100

    Django bug fixes

--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Sum
 from kde.models import KdeLookup
+import sys
 
 def index(request):
 
-    f2017 = KdeLookup.objects.filter(freq2017__isnull=False).distinct().count()
+    def str_to_class(classname):
+        return getattr(sys.modules[__name__], classname)
+
+    def distinct_count(column):
+        coln = str_to_class(column + (str(__isnull)))
+        dist_f = KdeLookup.objects.filter(coln=False).distinct().count()
+        return(dist_f)
+
+    f2017 = distinct_count('freq2017')
 
     print(f2017)
 
