adbbe0e46313e023e29682b30c0af9a9c14f463a
Justin <j.t.vandijk@ucl.ac.uk>
2018-07-31 14:07:49 +0100

    Set up settings modal

---
+++ webapp/static/js/modal.js
@@ -0,0 +1,18 @@
+// Settings modal
+var modal = document.getElementById('settings');
+var link = document.getElementById('mysettings_modal');
+var span = document.getElementsByClassName("close")[0];
+
+link.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
