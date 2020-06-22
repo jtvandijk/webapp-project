636a4d939c11cd8cf4a51e905170a5483071b17b
JT <jtvandijk@sun.ac.za>
2020-06-22 23:44:56 +0100

    update .geojson > json ref

--- gbnames/static/js/map.js
+++ gbnames/static/js/map.js
@@ -65,7 +65,7 @@ async function renderLayers(surname,years) {
 
   //scotland
   if (years.includes(1911)) {
-    var path = '/static/kde/sc/scotland/scotland_0.geojson';
+    var path = '/static/kde/sc/scotland/scotland_0.json';
     var scotland = await $j.getJSON(path,function(sct) {
 
       //individual polygons
